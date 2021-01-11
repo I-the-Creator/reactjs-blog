6b0531db60c11956d6ed5dc7d9c2eb8f75b88c98
I-the-Creator <andrey.burluckiy@gmail.com>
2021-01-11 10:08:39 +0100

    search results filtration add, states for newPost add, props for newPost add

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './About';
 import Missing from './Missing';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -34,10 +35,41 @@ function App() {
       body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
     }   
   ]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('');   // search term state
   const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
   const navigate = useNavigate();  // working with Browser History
 
+  // Search
+  useEffect(() => {
+    // filtering the posts by search term, searching in body and title 
+    const filteredResults = posts.filter(post => 
+      ((post.body).toLowerCase()).includes(search.toLowerCase())
+      || ((post.title).toLowerCase()).includes(search.toLowerCase()));
+
+      /*  set searchResults state by reversing filtered search results
+      so the newest posts will be at the top */
+      setSearchResults(filteredResults.reverse());
+;  }, [posts, search])
+
+  // submit new Post after creating - receives the event from submit button
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // if posts exists - get the last posts - get its id and increase by 1, if not just set as 1
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    // create new array with new post added - spread all existing posts and add new one
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);  // set the prop with new value
+    // set PostTitle and PostBody in the controlled inputs back to empty string after we've submitted
+    setPostTitle(''); 
+    setPostBody('');
+    navigate('/'); // back to homepage
+  }
+
+  // Deleting Post
   const handleDelete = (id) => {
     /*if the id got from postPage 'is not equal' post.id while iterating posts during filtering, 
     include it to postsList, otherwise filtering out post with id we have passed in */
@@ -51,17 +83,17 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout 
-        search={null}
-        setSearch={null}
+        search={search}
+        setSearch={setSearch}
       />}>
-        <Route index element={<Home posts={posts} />} />  {/* renders by default in place of Outlet */}
+        <Route index element={<Home posts={searchResults} />} />  {/* renders by default in place of Outlet */}
         <Route path="post">
           <Route index element={<NewPost   /* renders by default inside 'post' path */
-            handleSubmit={null}
-            postTitle={null}
-            setPostTitle={null}
-            postBody={null}
-            setPostBody={null}
+            handleSubmit={handleSubmit}
+            postTitle={postTitle}
+            setPostTitle={setPostTitle}
+            postBody={postBody}
+            setPostBody={setPostBody}
           />} />
           <Route path=":id" element={<PostPage   /* if we provided a post id with url: '/post/id', goes to useParams()*/
             posts={posts}
