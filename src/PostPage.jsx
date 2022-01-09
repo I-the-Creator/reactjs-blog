0e531aa4db9ed9d26344d5e94ff44688e7d775a2
I-the-Creator <andrey.burluckiy@gmail.com>
2022-01-09 15:58:26 +0100

    PostPage component content jsx and layout add

--- src/PostPage.jsx
+++ src/PostPage.jsx
@@ -1,7 +1,36 @@
-const PostPage = () => {
+import { useParams, Link } from "react-router-dom";
+
+const PostPage = ({ posts, handleDelete }) => {
+    const { id } = useParams();  // get 'id' from a path URL: from App.js router for this page
+    const post = posts.find(post => (post.id).toString() === id)  // get the post by id from posts array
+
     return (
-        <main>
-            <h1>PostPage</h1>
+        <main className="PostPage">
+            <article className="post">
+                {/*if post is exist */}
+                {post &&   
+                    <>
+                        <h2>{post.title}</h2>
+                        <p className="postDate">{post.datetime}</p>
+                        <p className="postBody">{post.body}</p>
+                        <button onClick={() => handleDelete(post.id)}>
+                            Delete Post
+                        </button>
+                    </>
+                }
+                {/*if post doesn't exist */}
+                {!post &&
+                    <>
+                        <h2>Post Not Found</h2>
+                        <p>Well, that's disappointing</p>
+                        <p>
+                            <Link to='/'>Visit Our Homepage</Link>
+                        </p>
+                    </>
+
+                }  
+            </article>
+
         </main>
     )
 }
